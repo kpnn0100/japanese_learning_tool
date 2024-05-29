023bb722ce5014e9bb88babeca5a2d47e4053e82
kpnn0100 <dnlnhtnam@gmail.com>
2024-05-29 10:21:04 +0700

    add n5

---
+++ word/groupt.py
@@ -0,0 +1,57 @@
+import csv
+import re
+import pandas as pd
+
+input_file = "/home/namdln/learning/japanese_learning_tool/word/n5_words.txt"
+output_file = "/home/namdln/learning/japanese_learning_tool/word/n5_words.csv"
+
+with open(input_file, "r") as file:
+    lines = file.readlines()
+
+dict = {
+    "kanji": [],
+    "furigana": [],
+    "romaji": [],
+    "meaning": []
+}
+
+df = pd.DataFrame(dict)
+for line in lines:
+    if len(line) <2:
+        continue
+    missing_kanji = False
+    if line.startswith(' '):
+        line = line[1:]
+    elif line.startswith('	'):
+        missing_kanji = True
+    line = line.replace('\t', '')
+    line = line.replace('\n', '')
+    data = re.sub(' +', ' ',line)
+    data = data.strip().split(" ")
+    if len(data) < 3:
+        continue
+    if len(data) < 4 and missing_kanji == False:
+        continue
+    i = 0
+
+    kanji = data[i]
+    if missing_kanji:
+        i = -1
+        kanji = " "
+    furigana = data[i+1]
+    romaji  = data[i+2]
+    meaning = " ".join(data[i+3:])
+    data = {
+        "kanji": kanji,
+        "furigana": furigana,
+        "romaji": romaji,
+        "meaning": meaning
+    }
+    df = df._append(data, ignore_index = True)
+
+with open(output_file, "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(["kanji", "furigana", "romaji", "meaning"])
+    writer.writerows(data)
+
+print("Conversion to CSV format is complete.")
